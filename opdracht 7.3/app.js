7720aab0efd2adfab3d033445da2580c7b4b9d3f
Marc-van-Elswijk <9018248@student.zadkine.nl>
2022-10-06 12:33:28 +0200

    Bonus

---
+++ opdracht 7.3/app.js
@@ -0,0 +1,11 @@
+var count = 0;
+var titel = document.getElementById ("titel")
+var tekst = document.getElementById ("h1")
+
+function clicker() {
+    count += 1;
+    tekst.innerText = count;
+    if (count == 10) {
+        count = -1;
+    }
+}
