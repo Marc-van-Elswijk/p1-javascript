0c0ff8b7651301d445c83dbef4123f161abf8ecb
Marc-van-Elswijk <9018248@student.zadkine.nl>
2022-10-06 11:42:24 +0200

    x

---
+++ opdracht 6.4/app.js
@@ -0,0 +1,17 @@
+function calculate(x, y) {
+    return x + y;
+}
+
+console.log (calculate(18, 51))
+
+function multiplyByFive (x, y) {
+    return x * y;
+}
+
+console.log (multiplyByFive (18, 51))
+
+function calculateMinutes(x) {
+    return x / 60;
+}
+
+console.log (calculateMinutes(3600))
