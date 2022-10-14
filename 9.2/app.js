4df7504de912d7959ab2231bc3406c51ad1572fd
Marc-van-Elswijk <9018248@student.zadkine.nl>
2022-10-14 09:19:57 +0200

    x

--- 9.2/app.js
+++ 9.2/app.js
@@ -1,16 +1,32 @@
 const dicht_aap = document.querySelector(".closed");
 const open_aap = document.querySelector(".open");
+let counter = 0;
+const text = document.getElementById("text")
 
 function openFace() {
     if (open_aap.classList.contains('active')) {
-        openFace.classList.add('active');
-        closedFace.classList.remove('active');
+        dicht_aap.classList.add('active');
+        open_aap.classList.remove('active');
+    }
+
+    counter = counter + 1
+    console.log(counter)
+
+    if (counter > 10) {
+text.innerText = "Lekker bezig"
     }
 }
 
 function closedFace() {
     if (dicht_aap.classList.contains('active')) {
-        openFace.classList.add('active');
-        closedFace.classList.remove('active');
+        open_aap.classList.add('active');
+        dicht_aap.classList.remove('active');
     }
+    counter = counter + 1
+    console.log(counter)
+    
+    if (counter > 10) {
+        text.innerText = "Lekker bezig"
+     }
 }
+
